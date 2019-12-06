3cea218d5325bd0ced6a11613de171603755717e
Sharon Watkins <swatkins876@gmail.com>
2019-12-06 16:49:01 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,18 @@
+class PigLatinizer 
+
+  def piglatinize(phrase) 
+    piglatinized = []
+    split_phrase = phrase.split()
+
+    split_phrase.each do |word|
+      if word[0].scan(/[aeiouAEIOU]/).count == 1 
+        piglatinized << word + 'way'
+      else   
+        cluster = word.split(/[aeiouAEIOU].*/)[0]
+        word.slice!(0 .. cluster.length-1)
+        piglatinized << word + cluster + 'ay' 
+      end 
+    end 
+
+    piglatinized.join(' ')
+  end
