c2c800e093ab7db236e52cc7c5137487e63bf2ed
Sharon Watkins <swatkins876@gmail.com>
2019-12-06 17:11:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,12 +3,12 @@ require_relative 'models/piglatinizer'
 
 class App < Sinatra::Base
 
-  get '/' do
+  get "/" do
     erb :user_input
   end
 
-  post '/piglatinize' do
-    @piglatinized = PigLatinizer.new
+  post "/piglatinize" do
+    @piglatinizer = PigLatinizer.new
     @user_phrase = params[:user_phrase]
     erb :piglatinize
   end
