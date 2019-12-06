6b369fb9db2739a6b63ab5dc5283286b7fbaeaf6
Sharon Watkins <swatkins876@gmail.com>
2019-12-06 17:12:29 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,19 +1,27 @@
 class PigLatinizer
+  attr_reader :user_phrase
 
-  def piglatinize(phrase)
-    piglatinized = []
-    split_phrase = phrase.split()
-
-    split_phrase.each do |word|
-      if word[0].scan(/[aeiouAEIOU]/).count == 1
-        piglatinized << word + 'way'
-      else
-        cluster = word.split(/[aeiouAEIOU].*/)[0]
-        word.slice!(0 .. cluster.length-1)
-        piglatinized << word + cluster + 'ay'
-      end
+    def initialize
+      @user_phrase = user_phrase
     end
 
-    piglatinized.join(' ')
-  end
+    def piglatinize(string)
+      result = []
+      word_array = string.split
+      word_array.map do |word|
+        letter_array = word.chars
+        if letter_array[0].downcase.start_with?('a','e','i','o','u')
+          letter_array << ["way"]
+          result << letter_array.join
+        else
+          until letter_array[0].downcase.start_with?('a','e','i','o','u') do
+            consonant = letter_array.shift
+            letter_array << consonant
+          end
+          letter_array << ["ay"]
+          result << letter_array.join
+        end
+      end
+      result.flatten.join(" ")
+    end
 end
