dce5d26058ce466b37c3dcddc7b73a4f07c0bc0c
Sharon Watkins <swatkins876@gmail.com>
2019-12-06 16:48:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @piglatinized = PigLatinizer.new
+    @user_phrase = params[:user_phrase]
+    erb :piglatinized
+  end
+
 end
